4d7f2429cce3f376d0b1be4838de59d92db32132
jarno <netletic@gmail.com>
2025-02-22 21:07:27 +0000

    ch8.3: working with session data

--- lets-go/snippetbox/cmd/web/handlers.go
+++ lets-go/snippetbox/cmd/web/handlers.go
@@ -37,8 +37,10 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
+
 	data := app.newTemplateData(r)
 	data.Snippet = snippet
+
 	app.render(w, r, http.StatusOK, "view.tmpl", data)
 }
 
@@ -82,6 +84,8 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		app.serverError(w, r, err)
 		return
 	}
+
+	app.sessionManager.Put(r.Context(), "flash", "Snippet successfully created!")
 
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
