d479dc4cf6e3ac51f80d669568fd915aa9ffb83d
jarno <netletic@gmail.com>
2025-02-22 23:05:21 +0000

    ch9.6: connection timeouts

--- lets-go/snippetbox/cmd/web/main.go
+++ lets-go/snippetbox/cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"database/sql"
 	"flag"
 	"fmt"
@@ -81,12 +82,36 @@ func main() {
 		sessionManager: sessionManager,
 	}
 
-	srv := &http.Server{
-		Addr:     *addr,
-		Handler:  app.Routes(),
-		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{
+			tls.X25519,
+			tls.CurveP256,
+		},
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+		},
+		MinVersion: tls.VersionTLS12,
+		MaxVersion: tls.VersionTLS13,
 	}
+
+	srv := &http.Server{
+		Addr:           *addr,
+		Handler:        app.Routes(),
+		ErrorLog:       slog.NewLogLogger(logger.Handler(), slog.LevelError),
+		TLSConfig:      tlsConfig,
+		IdleTimeout:    time.Minute,
+		ReadTimeout:    5 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 4096, // real total is 8192, Go adds 4096 bytes to this number by default
+	}
+
 	logger.Info("starting server", slog.String("addr", *addr))
+
 	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	logger.Error(err.Error())
 	os.Exit(1)
