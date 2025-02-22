4afaf99c239fc8f4b7af526cbf2554ac881e7bff
jarno <netletic@gmail.com>
2025-02-22 15:29:46 +0000

    ch7.2: parsing form data

--- lets-go/snippetbox/cmd/web/errors.go
+++ lets-go/snippetbox/cmd/web/errors.go
@@ -42,20 +42,36 @@ func (app *application) serverError(w http.ResponseWriter, r *http.Request, err 
 	http.Error(w, msg, status)
 }
 
-// func (app *application) clientError(w http.ResponseWriter, r *http.Request, status int) {
-// 	var (
-// 		method = r.Method
-// 		host   = r.Host
-// 		uri    = r.URL.RequestURI()
-// 	)
+func (app *application) clientError(w http.ResponseWriter, r *http.Request, status int) {
+	var (
+		ip     = r.RemoteAddr
+		method = r.Method
+		host   = r.Host
+		uri    = r.URL.EscapedPath()
+		proto  = r.Proto
+		scheme = "http"
+	)
 
-// 	app.logger.Error(
-// 		http.StatusText(status),
-// 		slog.String("method", method),
-// 		slog.String("host", host),
-// 		slog.String("uri", uri),
-// 		slog.Int("status", status),
-// 	)
+	traceID, ok := r.Context().Value("TraceID").(string)
+	if !ok {
+		traceID = ""
+	}
 
-// 	http.Error(w, http.StatusText(status), status)
-// }
+	if r.TLS != nil {
+		scheme = "https"
+	}
+
+	app.logger.Error(
+		http.StatusText(status),
+		slog.String("ip", ip),
+		slog.String("scheme", scheme),
+		slog.String("method", method),
+		slog.String("host", host),
+		slog.String("uri", uri),
+		slog.String("proto", proto),
+		slog.Int("status", status),
+		slog.String("trace_id", traceID),
+	)
+	msg := fmt.Sprintf("%s (X-Trace-Id: %s)", http.StatusText(status), traceID)
+	http.Error(w, msg, status)
+}
