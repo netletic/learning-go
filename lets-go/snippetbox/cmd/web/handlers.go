49216d875300fedc3aaf11afcaf2877d4a619a58
jarno <netletic@gmail.com>
2025-02-23 17:17:26 +0000

    ch10.5: user logout

--- lets-go/snippetbox/cmd/web/handlers.go
+++ lets-go/snippetbox/cmd/web/handlers.go
@@ -186,22 +186,25 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Use the RenewToken() method on the current session to change the session
-	// ID. It's good practice to generate a new session ID when the
-	// authentication state or privilege levels changes for the user (e.g. login
-	// and logout operations).
+	// renew session token to mitigate session fixation attacks
 	err = app.sessionManager.RenewToken(r.Context())
 	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
 
-	// Add the ID of the current user to the session, so that they are now
-	// 'logged in'.
+	// add the ID of the current user to the session, so that they are now 'logged in'.
 	app.sessionManager.Put(r.Context(), "authenticatedUserID", id)
 	http.Redirect(w, r, "/snippet/create", http.StatusSeeOther)
 }
 
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Logout the user...")
+	err := app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+	app.sessionManager.Put(r.Context(), "flash", "You've been logged out successfully!")
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
