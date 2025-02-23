e7626c715a23bb29f985b0c98ce81be71a57d3d8
jarno <netletic@gmail.com>
2025-02-23 17:00:05 +0000

    ch10.4: user login

--- lets-go/snippetbox/internal/models/users.go
+++ lets-go/snippetbox/internal/models/users.go
@@ -45,12 +45,36 @@ func (m *UserModel) Insert(name, email, password string) error {
 }
 
 func (m *UserModel) Authenticate(email, password string) (int, error) {
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	var id int
+	var hashedPassword []byte
+	stmt := `
+		SELECT id, hashed_password 
+		FROM users 
+		WHERE email = ?
+	`
+	row := m.DB.QueryRow(stmt, email)
+
+	// check that user matching given email exists
+	err := row.Scan(&id, &hashedPassword)
 	if err != nil {
-		return 1, err
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
 	}
-	_ = hashedPassword
-	return 0, nil
+
+	// user with that email exists, compare given password with password in db
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	return id, nil
 }
 
 func (m *UserModel) Exists(id int) (bool, error) {
